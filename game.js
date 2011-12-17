866783b9c41a424048d7918872243fbc304f4cf7
dzagidulin <dmitri@zagidulin.net>
2011-12-17 14:42:25 -0500

    OCD tweaking of whitespace

--- game.js
+++ game.js
@@ -10,7 +10,7 @@
 		var enemies = new jaws.SpriteList()
 
 		this.setup = function() {
-			player = new jaws.Sprite({image: "img/pixel_santa1.png", x: 10, y:100})
+			player = new jaws.Sprite({image: "img/santa_lucia.png", x: 10, y:100})
 			player.can_fire = true
 			jaws.on_keydown("esc",  function() { jaws.switchGameState(MenuState) })
 			jaws.preventDefaultKeys(["up", "down", "left", "right", "space"])
@@ -20,71 +20,69 @@
 			enemies.push(death)
 		}
  
-      this.update = function() {
-        if(jaws.pressed("left"))  { player.x -= 2 }
-        if(jaws.pressed("right")) { player.x += 2 }
-        if(jaws.pressed("up"))    { player.y -= 2 }
-        if(jaws.pressed("down"))  { player.y += 2 }
-        if(jaws.pressed("space")) { 
-          if(player.can_fire) {
-        	  bullet = new jaws.Sprite({image: "img/ornament_green.png", x: player.rect().right, y:player.y})
-        	  bullet.collision = false
+		this.update = function() {
+			if(jaws.pressed("left"))  { player.x -= 2 }
+			if(jaws.pressed("right")) { player.x += 2 }
+			if(jaws.pressed("up"))    { player.y -= 2 }
+			if(jaws.pressed("down"))  { player.y += 2 }
+			if(jaws.pressed("space")) { 
+				if(player.can_fire) {
+					bullet = new jaws.Sprite({image: "img/ornament_green.png", x: player.rect().right, y:player.y})
+					bullet.collision = false
 //            bullets.push( new Bullet(player.rect().right, player.y) )
-        	  bullets.push(bullet)
-            player.can_fire = false
-            setTimeout(function() { player.can_fire = true }, 100)
-          }
-        }
+					bullets.push(bullet)
+					player.can_fire = false
+					setTimeout(function() { player.can_fire = true }, 100)
+				}
+			}
  
-        forceInsideCanvas(player)
-        
-        bullets.forEach(function(sprite, index) {
-        	sprite.x += 8
+			forceInsideCanvas(player)
+
+			bullets.forEach(function(sprite, index) {
+				sprite.x += 8
 //        	if(sprite.rect().collideRect(death.rect())) {
 //        		sprite.collision = true
 //        	}
-        })
-        jaws.collideManyWithMany(bullets, enemies).forEach( function(pair, index) {
-          pair[0].collision = true
-          pair[1].collision = true
-        });
-        
-        bullets.deleteIf(isOutsideCanvas) // delete items for which isOutsideCanvas(item) is true
-        bullets.deleteIf(isHit)
-        enemies.deleteIf(isHit)
-        
-        fps.innerHTML = jaws.game_loop.fps
-      }
- 
-      this.draw = function() {
-        jaws.context.clearRect(0,0,jaws.width,jaws.height)
-        player.draw()
-        bullets.drawIf(isAlive)  // will call draw() on all items in the list
-        enemies.drawIf(isAlive)
+			})
+			jaws.collideManyWithMany(bullets, enemies).forEach( function(pair, index) {
+				pair[0].collision = true
+				pair[1].collision = true
+			});
 
-      }
+			bullets.deleteIf(isOutsideCanvas) // delete items for which isOutsideCanvas(item) is true
+			bullets.deleteIf(isHit)
+			enemies.deleteIf(isHit)
+
+			fps.innerHTML = jaws.game_loop.fps
+		}
  
-      function isHit(item) {
+		this.draw = function() {
+			jaws.context.clearRect(0,0,jaws.width,jaws.height)
+			player.draw()
+			bullets.drawIf(isAlive)  // will call draw() on all items in the list
+			enemies.drawIf(isAlive)
+		}
+ 
+		function isHit(item) {
 //    	  return item.rect.collideRect(death.rect)  
-    	  return item.collision
-      }
-      
-      function isAlive(item) {
-    	  return !isHit(item)
-      }
-      
-      
-      /* Simular to example1 but now we're using jaws properties to get width and height of canvas instead */
-      /* This mainly since we let jaws handle the canvas now */
-      function isOutsideCanvas(item) { 
-        return (item.x < 0 || item.y < 0 || item.x > jaws.width || item.y > jaws.height) 
-      }
-      function forceInsideCanvas(item) {
-        if(item.x < 0)                  { item.x = 0  }
-        if(item.right > jaws.width)     { item.x = jaws.width - item.width }
-        if(item.y < 0)                  { item.y = 0 }
-        if(item.bottom  > jaws.height)  { item.y = jaws.height - item.height }
-      }
+			return item.collision
+		}
+
+		function isAlive(item) {
+			return !isHit(item)
+		}
+
+/* Simular to example1 but now we're using jaws properties to get width and height of canvas instead */
+/* This mainly since we let jaws handle the canvas now */
+		function isOutsideCanvas(item) { 
+			return (item.x < 0 || item.y < 0 || item.x > jaws.width || item.y > jaws.height) 
+		}
+		function forceInsideCanvas(item) {
+			if(item.x < 0)                  { item.x = 0  }
+			if(item.right > jaws.width)     { item.x = jaws.width - item.width }
+			if(item.y < 0)                  { item.y = 0 }
+			if(item.bottom  > jaws.height)  { item.y = jaws.height - item.height }
+		}
  
 //      function Bullet(x, y) {
 //        this.x = x
@@ -97,13 +95,13 @@
 //      }
     }
  
-    /*
-    *
-    * MenuState is our lobby/welcome menu were gamer can chose start, high score and settings.
-    * For this example we have only implemented start. Start switches active game state by simply:
-    *   jaws.switchGameState(play)   (jaws.switchGameState(PlayState) would have worked too)
-    *
-    */
+/*
+*
+* MenuState is our lobby/welcome menu were gamer can chose start, high score and settings.
+* For this example we have only implemented start. Start switches active game state by simply:
+*   jaws.switchGameState(play)   (jaws.switchGameState(PlayState) would have worked too)
+*
+*/
 	function MenuState() {
 		var index = 0
  
@@ -141,16 +139,17 @@
 				this.drawText(14, fillStyle, itemText, 350, 350 + i * 40)
 			}
 		}
-    }
+	}
  
-    /*
-    *
-    * Our script-entry point
-    *
-    */
-    window.onload = function() {
-      jaws.assets.add("img/pixel_santa1.png")
-      jaws.assets.add("img/ornament_green.png")
-      jaws.assets.add("img/skull.png")
-      jaws.start(MenuState)
-    }
+/*
+*
+* Our script-entry point
+*
+*/
+	window.onload = function() {
+		jaws.assets.add("img/santa_clause.png")
+		jaws.assets.add("img/santa_lucia.png")
+		jaws.assets.add("img/ornament_green.png")
+		jaws.assets.add("img/skull.png")
+		jaws.start(MenuState)
+	}
